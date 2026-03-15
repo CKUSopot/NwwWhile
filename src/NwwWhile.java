0eb810715d7fe24d8fd492601fc5fb15cd773a00
Costa Robe <56robik@gmail.com>
2026-03-15 13:00:06 +0100

    ok

--- src/NwwWhile.java
+++ src/NwwWhile.java
@@ -1,15 +1,60 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+/**
+ * 13. Używając pętli while() napisz program NwwWhile, który będzie obliczał
+ * Najmniejszą Wspólną Wielokrotność (NWW) dwóch liczb wprowadzonych przez
+ * użytkownika (liczbaA i liczbaB) – czyli najmniejszą liczbę naturalną, która jest
+ * podzielna bez reszty przez obie liczby. Algorytm postępowania jest następujący:
+ * 1. NWW=liczbaA*liczbaB
+ * 2. zmiennaA=liczbaA
+ * 3. zmiennaB=liczbaB
+ * 4. Gdy zmiennaB<>0 wykonuj
+ * • zmiennaC=zmiennaB
+ * • zmiennaB= zmiennaA % zmiennaB
+ * • zmiennaA=zmiennaC
+ * • Idź do punktu 4
+ * 5. Wynik = Math.floor(NWW/zmiennaA)
+ */
+
+public class NwwWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean bylBlad;
+        do {
+            bylBlad = false;
+            try {
+                System.out.print("Podaj liczbę A: ");
+                int liczA = scanner.nextInt();
+                System.out.print("Podaj liczbę B: ");
+                int liczB = scanner.nextInt();
+                int nWW = liczA * liczB;
+                int zmienA = liczA;
+                int zmienB = liczB;
+                while (zmienB != 0){
+                    int zmienC = zmienB;
+                    zmienB = zmienA % zmienB;
+                    zmienA = zmienC;
+                }
+                double wynik = Math.floor(nWW/zmienA);
+                System.out.println(wynik);
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                bylBlad = true;
+            }
+        }while(bylBlad);
     }
 }
+
+// boolean bylBlad;
+//        do{
+//            bylBlad = false;
+//            try {
+//                System.out.print("Podaj liczbę: ");
+//                int liczba = scanner.nextInt();
+//
+//            } catch (Exception e) {
+//                scanner.nextLine();
+//                bylBlad = true;
+//            }
+//        }while(bylBlad);
